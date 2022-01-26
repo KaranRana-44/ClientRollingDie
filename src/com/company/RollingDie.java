a23015d023a3cae481acfb31aca7143873a11c11
student <1>
2022-01-26 12:02:45 -0500

    123

--- src/com/company/RollingDie.java
+++ src/com/company/RollingDie.java
@@ -1,17 +1,5 @@
 package com.company;
 
-/* A statistician is studying sequences of numbers obtained by repeatedly tossing a 6-sided die.
-The statistician is particularly interested in runs of numbers.  A run occurs when 2 or more
-consecutive tosses of the die produce the same value.  For example, in the following sequence of
-die rolls, there are runs starting at positions 1,6,12,and 14.
-
-Index:	0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17
-Result: 1  5  5  4  3  1  2  2  2  6  1  3  3  5  5  5  5  5
-
-The die is represented by the following class:
-*/
-
-import java.sql.SQLOutput;
 
 public class RollingDie {
     /** @return an integer value between 1 and 6
@@ -20,10 +8,6 @@ public class RollingDie {
     {
         return (int)(6 * Math.random()) + 1;
     }
-
-	/* Write the method getRolls that takes a number of tosses as a parameter.  The method
-	should return an array of values produced by rolling the die the given number of times.
-	*/
 
     /** Returns an array of the values obtained by tossing the die numTosses times.
      *  @param numTosses the number of die tosses
@@ -41,14 +25,6 @@ public class RollingDie {
         return rolls;  // replace this
     }
 
-	/*Write the method getLongestRun that takes as its parameter a array of integer values
-	representing a series of die tosses.  The method returns the starting index in the array
-	of a run of maximum size.  A run is defined as the repeated occurrence of the same value
-	in 2 or more consecutive positions in the array.  If there are 2 runs of the same size
-	it may return either index.  If there are no runs, the method returns -1.
-	Precondition:  values.length > 0
-	*/
-
     public int getBestRun(int[] values)
     {
         int count=0;
@@ -58,7 +34,7 @@ public class RollingDie {
         int currpos=0;
 
 
-        for(int i=1; i<values.length; i++){
+        for(int i=0; i<values.length; i++){
             if(values[i]==currval) {
                 count++;
                 }
@@ -73,7 +49,7 @@ public class RollingDie {
             }
         }
         if (prevcount==1){return -1;}
-        return prevpos+1;
+        return prevpos;
 
           //replace this
     }
@@ -93,7 +69,11 @@ public class RollingDie {
         }
 
         System.out.println("non-repeating dice set(very low chance of really happening)");
-        int[] x= {1,2,3,4,5,6,7,8,9,1};
+        int[] x= {1,2,3,4,5,6,5,4,3,2,1};
+        for (int i = 0; i < x.length; i++) {
+            System.out.print(x[i] + ",");
+        }
+        System.out.println();
         int y=nc.getBestRun(x);
         System.out.println(y);
 
